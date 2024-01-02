a80e9688bb41bd837606ae3875253098ef623180
Selwyn Simsek <selwyn.simsek@cantab.net>
2024-01-02 12:14:35 +0000

    changes

--- src/main.lisp
+++ src/main.lisp
@@ -24,9 +24,14 @@
 
 (defmacro echo (&rest body)
   "Evaluates the forms in body and prints the results to XMPP."
-  (let ((form-name (gensym)))
-    `(let ((,form-name (multiple-value-list (progn ,@body))))
-       (write-to-xmpp ,form-name))))
+  (let ((form-name (gensym))
+        (form-value (gensym))
+        (body-name (gensym)))
+    `(progn
+       (loop for ,body-name in ',body do (write-to-xmpp ,body-name))
+       (write-to-xmpp "=>")
+       (let ((,form-name (multiple-value-list (progn ,@body))))
+         (loop for ,form-value in ,form-name do (write-to-xmpp ,form-value))))))
 
 (export 'echo)
 ;; blah blah blah.
